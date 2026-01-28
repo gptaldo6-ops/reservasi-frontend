b8ef471a7ac257eaa91294b76919dfd9366f96a9
gptaldo6-ops <gptaldo6@gmail.com>
2026-01-28 08:55:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ paketRadios.forEach(radio => {
   });
 });
 
+const paketCards = document.querySelectorAll(".paket-card");
+
+paketCards.forEach(card => {
+  let qty = 0;
+  const qtySpan = card.querySelector(".qty");
+  const btnPlus = card.querySelector(".btn-plus");
+  const btnMinus = card.querySelector(".btn-minus");
+
+  btnPlus.addEventListener("click", () => {
+    qty++;
+    qtySpan.textContent = qty;
+  });
+
+  btnMinus.addEventListener("click", () => {
+    if (qty > 0) qty--;
+    qtySpan.textContent = qty;
+  });
+});
+
+
