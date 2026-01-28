b0dc6061d43da510b880cbf2015737e39ac99395
gptaldo6-ops <gptaldo6@gmail.com>
2026-01-28 08:40:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,9 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwFU-fHZR5lphEAX0R-I_BvKQx5H1MtCBxgfQU7s6Xnc-RYgx3UZX61RY7eXshk3EX0Sw/exec";
+const paketRadios = document.querySelectorAll('input[name="paket"]');
+const paketDetailBox = document.getElementById("paket-detail");
 
-document.getElementById("btnTest").addEventListener("click", async () => {
-  const payload = {
-    nama: "TEST VERCEL",
-    whatsapp: "08123456789",
-    tanggal: "2026-01-26",
-    tableId: "KANAN-1",
-    paketA: 1,
-    paketB: 0,
-    paketC: 0
-  };
-
-  try {
-    const res = await fetch(API_URL, {
-    method: "POST",
-    body: JSON.stringify(payload)
-    });
-
-
-    const data = await res.json();
-    document.getElementById("output").textContent =
-      JSON.stringify(data, null, 2);
-
-  } catch (err) {
-    document.getElementById("output").textContent =
-      "ERROR: " + err.message;
-  }
+paketRadios.forEach(radio => {
+  radio.addEventListener("change", () => {
+    paketDetailBox.style.display = "block";
+  });
 });
 
